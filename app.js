737b7c786e102cea301cc3daf2dbb9ade027b6d8
hello-aditi <119799820+hello-aditi@users.noreply.github.com>
2024-02-26 19:52:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ var eventApp = angular.module('eventApp', ['ngRoute']);
 
 eventApp.config(function ($routeProvider) {
     $routeProvider.when("/home", {
-        templateUrl: 'views/home.htm',
+        templateUrl: 'home.htm',
         controller: 'homeCtrl'
     })
         .when("/registration", {
-            templateUrl: 'views/registration.htm',
+            templateUrl: 'registration.htm',
             controller: 'registrationCtrl'
         })
         .when("/events", {
-            templateUrl: 'views/events.htm',
+            templateUrl: 'events.htm',
             controller: 'eventCtrl'
         })
         .when("/gallery", {
-            templateUrl: 'views/gallery.htm',
+            templateUrl: 'gallery.htm',
             controller: 'galleryCtrl'
         })
         .when("/logout", {
-            templateUrl: 'views/logout.html',
+            templateUrl: 'logout.html',
         })
         .otherwise({ redirectTo: "/home" })
 });
